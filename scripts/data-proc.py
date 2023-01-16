7f7d1e3acba3b26492cfb57715aa17be23724ff8
theodorross <theodor.a.ross@uit.no>
2023-01-16 14:15:07 +0100

    updating data preproc

--- scripts/data-proc.py
+++ scripts/data-proc.py
@@ -112,9 +112,9 @@ if __name__ == "__main__":
     ## Extract and preprocess datasets with the desired antibiotics.
     antibiotics = ["Ceftazidim", "Ciprofloxacin", "Gentamicin"]
 
-    NORM_df = extractNORMdata("../data/raw-spreadsheets/per_isolate_AST_DD_SIR_v4.xlsx", *antibiotics)
-    UTI_df = extractUTIdata("../data/raw-spreadsheets/20220324_E. coli NORM urin 2000-2021_no_metadata[2].xlsx", *antibiotics)
+    NORM_df = extractNORMdata("./data/raw-spreadsheets/per_isolate_AST_DD_SIR_v4.xlsx", *antibiotics)
+    UTI_df = extractUTIdata("./data/raw-spreadsheets/20220324_E. coli NORM urin 2000-2021_no_metadata[2].xlsx", *antibiotics)
 
     ## Save the processed data to new files.
-    NORM_df.to_csv("../data/processed-spreadsheets/NORM_data.csv")
-    UTI_df.to_csv("../data/processed-spreadsheets/UTI_data.csv")
+    NORM_df.to_csv("./data/processed-spreadsheets/NORM_data.csv")
+    UTI_df.to_csv("./data/processed-spreadsheets/UTI_data.csv")
