04ba3499e33462bce1e64243eadd469bffbb2f6f
theodorross <theodor.a.ross@uit.no>
2025-02-28 16:34:32 +0100

    cleaning repo

--- create-env.sh
+++
@@ -1,7 +0,0 @@
-conda create -n ecoli-uti python=3.10.10 scipy=1.10.1 scikit-learn=1.2.2 \
-    pandas=1.5.3 xgboost=1.7.4 matplotlib=3.7.1 altair=5.0.0 openpyxl=3.1.2 \
-    nodejs=18.15.0 vega-cli=5.24.0 vl-convert-python=0.11.2
-
-# conda create -n ecoli-uti python=3.8.8 scipy=1.10.0 scikit-learn=0.24.2 \
-#     pandas=1.3.2 xgboost=1.5.1 matplotlib=3.4.2 altair=4.2.0 openpyxl=3.0.7 \
-#     nodejs vega-lite-cli=4.17.0 vega-cli=5.17.0 altair_saver=0.5.0
